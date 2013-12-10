36a8dc793d5c5ddf956fe194e5ce36c6423d36df
andrekahirata <andre.kengi@gmail.com>
2013-12-10 11:59:21 -0800

    Esqueleto Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FisrApp::Application.config.secret_token = 'd83c8647f49bf000fa357dd1d755a0535a6625df27de8caddce2dc1812acad5a758c15d91d1bc3c2dd0b84f52ab309402cb27749100ac0e944940e744ce649f4'
